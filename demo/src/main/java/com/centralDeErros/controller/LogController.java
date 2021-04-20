86e546825715bd807b6e3812948d2622246ff0f8
mateus <mateuslaalmeida@gmail.com>
2021-04-20 12:04:18 -0300

    Inicio da configuração do DTO

--- demo/src/main/java/com/centralDeErros/controller/LogController.java
+++ demo/src/main/java/com/centralDeErros/controller/LogController.java
@@ -3,16 +3,15 @@ package com.centralDeErros.controller;
 import com.centralDeErros.entity.Log;
 import com.centralDeErros.service.impl.LogService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 
-import java.util.Comparator;
 import java.util.List;
 
 @RestController
@@ -23,9 +22,13 @@ public class LogController {
     private LogService logService;
 
     @GetMapping
-    public List<Log> findAllLogs(@RequestParam(defaultValue = "0") Integer pageNo, @RequestParam(defaultValue = "10") Integer pageSize, @RequestParam(defaultValue = "id") String sortBy) {
+    public List<Log> findAllLogs(@RequestParam(defaultValue = "0") Integer pageNo,
+                                 @RequestParam(defaultValue = "10") Integer pageSize,
+                                 @RequestParam(defaultValue = "id") String sortBy) {
         Pageable paging = (PageRequest.of(pageNo, pageSize, Sort.by(sortBy)));
         List<Log> list = logService.findAll(paging);
         return list;
     }
+
+
 }
