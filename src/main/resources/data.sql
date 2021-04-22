9d933cb37fe7bd192cf562a2387c3f4b1138a71a
Mateus Leite Alves de Almeida <mateuslaalmeida@gmail.com>
2021-04-22 17:52:01 -0300

    Delete src directory

--- src/main/resources/data.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO users
-VALUES
-(1, 'email@email.com', 'Donald Duck', 'senha123');
-
-INSERT INTO logs
-VALUES
-(1, '2021-04-18', 'sem comunicação com o banco', 'ERROR', '401', 'mobile'),
-(2, '2021-04-18T10:00:00', 'perda de dados secundários', 'WARNING', '401', 'desktop'),
-(3, '2021-04-18T11:00:00', 'nome do usuário não informado', 'INFO', '401', 'web');
