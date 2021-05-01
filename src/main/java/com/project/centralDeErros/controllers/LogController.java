1fac911645189d8e1e1c081ec3c2116a49a76e5f
mateus <mateuslaalmeida@gmail.com>
2021-05-01 16:10:45 -0300

    Movidos os conversores de dtos para o service implementation e alterados os retornos dos logs para pages ao inves de listas

--- src/main/java/com/project/centralDeErros/controllers/LogController.java
+++ src/main/java/com/project/centralDeErros/controllers/LogController.java
@@ -1,17 +1,14 @@
 package com.project.centralDeErros.controllers;
 
-
-import com.project.centralDeErros.converter.LogConverter;
 import com.project.centralDeErros.dto.LogDto;
 import com.project.centralDeErros.entity.Level;
 import com.project.centralDeErros.entity.Log;
-import com.project.centralDeErros.entity.User;
-import com.project.centralDeErros.repository.LogRepository;
 import com.project.centralDeErros.services.impl.LogService;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
@@ -19,7 +16,6 @@ import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
-import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -29,18 +25,14 @@ public class LogController {
     @Autowired
     private LogService logService;
 
-    @Autowired
-    LogConverter converter;
-
     @GetMapping
     @ApiOperation("Lista todos os logs de erros")
     @ApiResponses(value = {@ApiResponse(code = 200, message = "Logs retornados com sucesso")})
-    public List<LogDto> findAllLogs(@RequestParam(defaultValue = "0") Integer pageNo,
+    public Page<LogDto> findAllLogs(@RequestParam(defaultValue = "0") Integer pageNo,
                                     @RequestParam(defaultValue = "10") Integer pageSize,
                                     @RequestParam(defaultValue = "id") String sortBy) {
         Pageable paging = (PageRequest.of(pageNo, pageSize, Sort.by(sortBy)));
-        List<Log> list = logService.findAll(paging);
-        return converter.entityToDto(list);
+        return logService.findAll(paging);
     }
 
     @GetMapping("/id/{id}")
@@ -55,36 +47,32 @@ public class LogController {
     @ApiOperation("Lista os logs de erros pelo level")
     @ApiResponses(value = {@ApiResponse(code = 404, message = "Logs não encontrados"),
             @ApiResponse(code = 200, message = "Logs retornados com sucesso")})
-    public List<LogDto> findByLevel(@PathVariable Level level, Pageable pageable) {
-        List<Log> list = logService.findByLevel(level, pageable);
-        return converter.entityToDto(list);
+    public Page<LogDto> findByLevel(@PathVariable Level level, Pageable pageable) {
+        return logService.findByLevel(level, pageable);
     }
 
     @GetMapping("/description/{description}")
     @ApiOperation("Lista os logs de erros pela descrição")
     @ApiResponses(value = {@ApiResponse(code = 404, message = "Logs não encontrados"),
             @ApiResponse(code = 200, message = "Logs retornados com sucesso")})
-    public List<LogDto> findByDescription(@PathVariable String description, Pageable pageable) {
-        List<Log> list = logService.findByDescriptionContaining(description, pageable);
-        return converter.entityToDto(list);
+    public Page<LogDto> findByDescription(@PathVariable String description, Pageable pageable) {
+        return logService.findByDescriptionContaining(description, pageable);
     }
 
     @GetMapping("/origin/{origin}")
     @ApiOperation("Lista os logs de erros pela origem")
     @ApiResponses(value = {@ApiResponse(code = 404, message = "Logs não encontrados"),
             @ApiResponse(code = 200, message = "Logs retornados com sucesso")})
-    public List<LogDto> findByOrigin(@PathVariable String origin, Pageable pageable) {
-        List<Log> list = logService.findByOrigin(origin, pageable);
-        return converter.entityToDto(list);
+    public Page<LogDto> findByOrigin(@PathVariable String origin, Pageable pageable) {
+        return logService.findByOrigin(origin, pageable);
     }
 
     @GetMapping("/date/{date}")
     @ApiOperation("Lista os logs de erros pela data")
     @ApiResponses(value = {@ApiResponse(code = 404, message = "Logs não encontrados"),
             @ApiResponse(code = 200, message = "Logs retornados com sucesso")})
-    public List<LogDto> findByDate(@PathVariable @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate date, Pageable pageable) {
-        List<Log> list = logService.findByDate(date, pageable);
-        return converter.entityToDto(list);
+    public Page<LogDto> findByDate(@PathVariable @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate date, Pageable pageable) {
+        return logService.findByDate(date, pageable);
     }
 
     @PostMapping("/register")
