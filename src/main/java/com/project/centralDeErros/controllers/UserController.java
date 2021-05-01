5b02675d83c43662ac1828127f984c4851385ca6
mateus <mateuslaalmeida@gmail.com>
2021-04-30 22:23:47 -0300

    Adicionado swagger

--- src/main/java/com/project/centralDeErros/controllers/UserController.java
+++ src/main/java/com/project/centralDeErros/controllers/UserController.java
@@ -3,6 +3,9 @@ package com.project.centralDeErros.controllers;
 import com.project.centralDeErros.entity.User;
 import com.project.centralDeErros.repository.UserRepository;
 import com.project.centralDeErros.services.impl.UserService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -22,16 +25,22 @@ public class UserController {
     private UserRepository userRepository;
 
     @GetMapping
+    @ApiOperation("Lista todos os usuários")
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "Usuários retornados com sucesso")})
     public List<User> findAllUsers() {
         return userService.findAll();
     }
 
     @GetMapping("/email/{email}")
+    @ApiOperation("Valida se o usuário existe")
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "Usuários retornados com sucesso")})
     public Optional<User> findUserByEmail(@PathVariable("email") String email) {
         return userService.findByEmail(email);
     }
 
     @PostMapping("/register")
+    @ApiOperation("Cria um novo usuário")
+    @ApiResponses(value = {@ApiResponse(code = 201, message = "Usuário criado com sucesso")})
     public User registerUser (@RequestBody User user) {
         return userService.save(user);
     }
