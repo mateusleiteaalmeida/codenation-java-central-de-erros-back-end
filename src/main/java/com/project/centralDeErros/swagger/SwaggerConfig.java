6c6b441768fe60c0e52c301839b0cd47ec5c222b
mateus <mateuslaalmeida@gmail.com>
2021-05-11 13:18:17 -0300

    Adicionada configuração no swagger para gerar documentação

--- src/main/java/com/project/centralDeErros/swagger/SwaggerConfig.java
+++ src/main/java/com/project/centralDeErros/swagger/SwaggerConfig.java
@@ -2,14 +2,17 @@ package com.project.centralDeErros.swagger;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 @Configuration
 @EnableSwagger2
@@ -21,19 +24,27 @@ public class SwaggerConfig {
                 .apis(RequestHandlerSelectors.basePackage("com.project.centralDeErros"))
                 .paths(PathSelectors.any())
                 .build()
-                .apiInfo(apiInfo())
-                .useDefaultResponseMessages(false);
+                .globalOperationParameters(Collections.singletonList(new ParameterBuilder()
+                    .name("Authorization")
+                    .description("Bearer token")
+                    .modelRef(new ModelRef("string"))
+                    .parameterType("header")
+                    .defaultValue("Bearer")
+                    .required(true)
+                    .build()))
+                    .apiInfo(apiInfo())
+                    .useDefaultResponseMessages(false);
     }
 
     private ApiInfo apiInfo() {
         return new ApiInfo(
-                "Nome",
-                "Descrição",
-                "Versão",
+                "Central de erros",
+                "Aplicação para centralização de registros de erros.",
+                "1.0",
                 "Termos de serviço",
                 null,
-                "Licença",
-                "URL da licença",
+                null,
+                null,
                 new ArrayList<>()
         );
 
